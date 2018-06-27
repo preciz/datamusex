2a7685be1dd972bdb6683e2992dadb96153d35f6
preciz <barna.kovacs.codes@gmail.com>
2018-06-27 21:41:23 +0200

    Fix suggestions function, allow passing of headers

--- lib/datamusex.ex
+++ lib/datamusex.ex
@@ -5,28 +5,44 @@ defmodule Datamusex do
 
   use HTTPoison.Base
 
-  endpoints = [
+  words_endpoints = [
     similar_meaning: :ml,
     sound_like: :ml,
     spelled_similarly: :sp,
     rhyme_with: :rel_rhy,
     used_to_describe: :rel_jjb,
     often_follow: :lc,
-    triggered_by: :rel_trg,
-    suggestions: :sug
+    triggered_by: :rel_trg
   ]
 
-  endpoints
+  words_endpoints
   |> Enum.each(fn {name, query_param} ->
-    def unquote(name)(words, extra_params \\ [])
-        when is_binary(words) and is_list(extra_params) do
+    def unquote(name)(words, headers \\ [], options \\ [])
+        when is_binary(words) and is_list(headers) and is_list(options) do
+      default_params = [{unquote(query_param), process_words(words)}]
+
+      options = options |> Keyword.update(:params, default_params, &(&1 ++ default_params))
+
       get(
-        "",
-        [],
-        params: [{unquote(query_param), words |> process_words}] ++ extra_params
+        "words",
+        headers,
+        options
       )
     end
   end)
+
+  def suggestions(words, headers \\ [], options \\ [])
+      when is_binary(words) and is_list(headers) and is_list(options) do
+    default_params = [s: process_words(words)]
+
+    options = options |> Keyword.update(:params, default_params, &(&1 ++ default_params))
+
+    get(
+      "sug",
+      headers,
+      options
+    )
+  end
 
   def process_words(words) when is_binary(words) do
     words
@@ -35,7 +51,7 @@ defmodule Datamusex do
   end
 
   def process_url(url) do
-    "https://api.datamuse.com/words" <> url
+    "https://api.datamuse.com/" <> url
   end
 
   def process_response_body(body) do
