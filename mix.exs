152b96b038651ed1da4423384b5b325615629f8f
preciz <barna.kovacs.codes@gmail.com>
2018-06-27 21:41:45 +0200

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Datamusex.MixProject do
   def project do
     [
       app: :datamusex,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: "Datamuse API wrapper",
