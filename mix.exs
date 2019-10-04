61f16123a5cb1f5172ef3d53d8dacb9445473b67
preciz <barna.kovacs.codes@gmail.com>
2019-10-04 12:36:14 +0200

    Add ex_doc package to deps and docs() function to mix.exs

--- mix.exs
+++ mix.exs
@@ -1,16 +1,18 @@
 defmodule Datamusex.MixProject do
   use Mix.Project
 
+  @version "0.3.0"
   @github "https://github.com/preciz/datamusex"
 
   def project do
     [
       app: :datamusex,
-      version: "0.3.0",
+      version: @version,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: "Datamuse API wrapper",
       deps: deps(),
+      docs: docs(),
       package: package(),
       name: "Datamusex"
     ]
@@ -25,7 +27,16 @@ defmodule Datamusex.MixProject do
   defp deps do
     [
       {:jason, "~> 1.1"},
-      {:httpoison, "~> 1.0"}
+      {:httpoison, "~> 1.0"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Datamusex",
+      source_ref: "v#{@version}",
+      source_url: @github,
     ]
   end
 
